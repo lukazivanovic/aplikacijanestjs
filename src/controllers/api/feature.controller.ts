785867b614cc7daed4ae36e94651c9b8da3cc035
lukazivanovic <sdluka@hotmail.com>
2020-08-18 23:58:53 +0200

    Create the featuer management mechanism

---
+++ src/controllers/api/feature.controller.ts
@@ -0,0 +1,34 @@
+import { Controller } from "@nestjs/common";
+import { Crud } from "@nestjsx/crud";
+import { Feature } from "src/entities/feature.entity";
+import { FeatureService } from "src/services/feature/feature.service";
+
+@Controller('api/feature')
+@Crud({
+    model: {
+        type: Feature
+    },
+    params: {
+        id: {
+            field: 'featureId',
+            type: 'number',
+            primary: true
+        }
+    },
+    query: {
+        join: {
+            articleFeatures: {
+                eager: false
+            },
+            category: {
+                eager: true
+            },
+            articles: {
+                eager: false
+            }
+        }
+    }
+})
+export class FeatureController {
+    constructor(public service: FeatureService) { }
+}
