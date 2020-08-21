8f0919a6e4d86b8b22716d4bccbbd48134bac1b9
lukazivanovic <sdluka@hotmail.com>
2020-08-21 16:43:43 +0200

    Implement e-mail sending features

---
+++ config/mail.config.ts
@@ -0,0 +1,8 @@
+export const MailConfig = {
+    hostname: 'smtp.gmail.com',
+    username: 'djellet',
+    password: 'hradoibghpikuhyl',
+    senderEmail: 'djellet@gmail.com',
+
+    orderNotificationMail: 'sdluka@hotmail.com',
+}
